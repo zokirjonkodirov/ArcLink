c74eb236d7a49960f69fc041504b6ad71951ab8c
Zokir <zqodirov@cdti.uz>
2026-03-13 14:40:10 +0500

    Update formula with correct SHA256

--- Formula/arclink.rb
+++ Formula/arclink.rb
@@ -2,7 +2,7 @@ class Arclink < Formula
   desc "Export Arc Browser pinned tabs to Markdown"
   homepage "https://github.com/zokirjonkodirov/ArcLink"
   url "https://github.com/zokirjonkodirov/ArcLink/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  sha256 "ebabdff3607e14661ff68a43a62842c6954bd110c74b2cb80dee4a666ae9e670"
   license "MIT"
   version "1.0.0"
 
